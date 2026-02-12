f04a21570125fcd722a156e0ce7d4e60217e761f
jhaeylscode <jhonlestermagpayo@gmail.com>
2026-02-12 17:17:21 +0800

    Initial commit

---
+++ resources/js/Pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="p-4 text-center bg-blue-700 ">
+            <h1 className="text-2xl font-bold mb-4 text-center">Welcome to the JhaelHomePages</h1>
+            <p>Welcome to my Page Home</p>
+        </div>
+    );
+}
